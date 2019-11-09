c9dcae335c227fea1ccc878a05fe7bedc2ef4aaf
JoshuaManto <joshuamanto@yahoo.com>
2019-11-09 14:39:03 -0800

    Routes post request '/addevent' to addEvent controller

---
+++ server/routers/EventRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const eventController = require('../controllers/EventController');
+
+const router = express.Router();
+
+router.post('/addevent', eventController.addEvent, (req, res) => {
+  console.log('in router add event');
+  if (res.locals.success) {
+    res.status(200).json(res.locals.event);
+  } else res.status(400).json(res.locals.error);
+});
+
+module.exports = router;

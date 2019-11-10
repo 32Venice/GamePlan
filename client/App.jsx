5e2d69a086b07dd3a4d5c692f9e9c8e06e840057
JoshuaManto <joshuamanto@yahoo.com>
2019-11-10 15:46:14 -0800

    Commented out handler and state binding. Need frontend to test

--- client/App.jsx
+++ client/App.jsx
@@ -8,7 +8,102 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    // this.state = {
+    //   event_id: '5dc73845e648f477e4848644',
+    //   items: [],
+    //   itemToBeAdded: ''
+    // };
+
+    // this.addItem = this.addItem.bind(this);
+    // this.deleteItem = this.deleteItem.bind(this);
   }
+
+  // Add back to create when frontend component is done to be able to test
+  // addItem(e) {
+  //   axios
+  //     .post(
+  //       '/shoppinglist/addItem',
+  //       {
+  //         item: this.state.itemToBeAdded,
+  //         event_id: this.state.event_id
+  //       },
+  //       {
+  //         headers: {
+  //           'Access-Control-Allow-Origin': '*',
+  //           'Content-Type': 'application/json'
+  //         }
+  //       }
+  //     )
+  //     .then(res => {
+  //       console.log(res);
+
+  //       this.setState({
+
+  //       })
+  //     });
+
+  // e.preventDefault();
+  // console.log(“in frontend “, this.todo.value);
+  // fetch(“/add”, {
+  //   method: “POST”,
+  //   headers: { “Content-Type”: “application/json” },
+  //   body: JSON.stringify({ item: this.todo.value })
+  // })
+  //   .then(res => {
+  //     this.todo.value = “”;
+  //     return res.json();
+  //   })
+  //   .catch(err => console.log(“ShoppingContainer: fetch POST ERROR: “, err));
+  // this.setState({
+  //   items: [this.todo.value, ...this.state.items]
+  // });
+  // }
+  // deleteItem(val) {
+  //   console.log(“MY VAL!!!!!!!!!!!“, val);
+  //   fetch(“/delete”, {
+  //     method: “DELETE”,
+  //     headers: { “content-type”: “application/json” },
+  //     body: JSON.stringify({ item: val })
+  //   })
+  //     .then(res => {
+  //       // console.log(“My res -->“, res);
+  //       location.reload();
+  //       return res.json();
+  //     })
+  //     .catch(err => console.log(“TodoItems: fetch DELETE ERROR: “, err));
+  // }
+  // componentWillMount() {
+  //   fetch(“/getAll”)
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       for (let i = data.length - 1; i >= 0; i--) {
+  //         myListItems.push(
+  //           <div>
+  //             <button
+  //               id=“compbutton”
+  //               key={i}
+  //               onClick={() => this.deleteItem(data[i].item)}
+  //             >
+  //               Complete Task
+  //             </button>
+  //             <div
+  //               className=“eachItem”
+  //               // key={i}
+  //               id={i}
+  //               // onClick={() => this.deleteItem(data[i].item)}
+  //             >
+  //               {data[i].item}
+  //             </div>
+  //           </div>
+  //         );
+  //       }
+  //       this.setState({ promiseIsResolved: true });
+  //       // console.log(“myListItems in fetch --> “, myListItems);
+  //     })
+  //     .catch(err => console.log(“ShoppingContainer: fetch GET ERROR: “, err));
+  // }
 
   render() {
     const Guests = [];

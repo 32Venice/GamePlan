1dd4850f615042ad6f94f157659ee156aeca2be3
JoshuaManto <joshuamanto@yahoo.com>
2019-11-10 15:43:37 -0800

    Merged from Talya's code for Activity routing

---
+++ server/routers/ActivitiesRouter.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const activitiesController = require("../controllers/ActivitiesController");
+
+const router = express.Router();
+
+router.get("/", activitiesController.getItems, (req, res) =>
+  res.status(200).json(res.locals.activity)
+);
+
+router.post("/addItem", activitiesController.addItem, (req, res) => {
+  console.log("in Post Router");
+  if (res.locals.success) {
+    res.status(200).json(res.locals.activity);
+  } else {
+    res.status(400).json(res.locals.error);
+  }
+});
+
+// router.put("/updateItem", activitiesController.updateItem, (req, res) =>
+//   res.status(200).json([...res.locals.activity])
+// );
+
+router.delete("/deleteItem", activitiesController.deleteItem, (req, res) =>
+  res.status(200).json(res.locals.activity)
+);
+
+module.exports = router;
